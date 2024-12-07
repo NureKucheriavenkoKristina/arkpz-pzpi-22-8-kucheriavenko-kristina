c00e6dc7224e97dd3277d195fca2d28372423a1d
Kristina <116723770+NureKucheriavenkoKristina@users.noreply.github.com>
2024-12-07 21:17:30 +0200

    Update EventLog.java

--- Task2/arkpz-pzpi-22-8-kucheriavenko-kristina-task2/model/EventLog.java
+++ Task2/arkpz-pzpi-22-8-kucheriavenko-kristina-task2/model/EventLog.java
@@ -18,31 +18,19 @@ public class EventLog {
     private Long eventLogID;
 
     @NotNull(message = "Action cannot be null")
-    @Size(min = 2, max = 255, message = "Action must be between 2 and 255 characters")
+    @Size(min = 2, max = 1000, message = "Action must be between 2 and 255 characters")
     @Column(nullable = false)
-    private String action;
+    private String actionDetails;
 
     @NotNull(message = "Action time cannot be null")
     @PastOrPresent(message = "Action time must be in the past or present")
     @Column(nullable = false)
     private Date actionTime;
 
-    @NotNull(message = "Details cannot be null")
-    @Size(min = 5, max = 1000, message = "Details must be between 5 and 1000 characters")
-    @Column(nullable = false)
-    private String details;
-
     @ManyToOne
     @JoinColumn(name = "userID")
     @NotNull(message = "Creator (user) cannot be null")
     private User creatorID;
 
-    @ManyToOne
-    @JoinColumn(name = "materialID")
-    @NotNull(message = "Biological material cannot be null")
-    private BiologicalMaterial materialID;
 
-    @OneToMany
-    @JoinColumn(name = "reportID")
-    private List<Report> reports;
 }
