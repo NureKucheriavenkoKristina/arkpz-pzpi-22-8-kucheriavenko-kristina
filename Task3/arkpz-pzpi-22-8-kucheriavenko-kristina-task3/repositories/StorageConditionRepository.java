06a7d6deb10be4ec028a867f20b6e5c515191505
Kristina <116723770+NureKucheriavenkoKristina@users.noreply.github.com>
2024-12-16 16:51:50 +0200

    Delete Task3/arkpz-pzpi-22-8-kucheriavenko-kristina-task3/repositories directory

--- Task3/arkpz-pzpi-22-8-kucheriavenko-kristina-task3/repositories/StorageConditionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BiologicalMaterialsSystem.repositories;
-
-import com.BiologicalMaterialsSystem.model.StorageCondition;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StorageConditionRepository extends JpaRepository<StorageCondition, Long> {
-}
